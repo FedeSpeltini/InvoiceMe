67b359bddd4ac421f90da2f8945e45503b7c830d
Federico Speltini <nicolas.speltini@masglobalconsulting.com>
2023-01-26 09:12:06 -0300

    Add project files.

---
+++ Facturame/ManagerPdf.cs
@@ -0,0 +1,131 @@
+﻿using PdfSharpCore.Drawing;
+using PdfSharpCore.Pdf;
+using PdfSharpCore.Pdf.Advanced;
+using PdfSharpCore.Pdf.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using static ICSharpCode.SharpZipLib.Zip.ExtendedUnixData;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+using static PdfSharpCore.Pdf.AcroForms.PdfAcroField;
+using PdfSharpCore.Fonts;
+using PdfSharpCore.Utils;
+using System.Runtime.InteropServices;
+
+namespace Facturame
+{
+    public class ManagerPdf
+    {
+        private readonly PdfDocument document;
+        private readonly PdfPage page;
+        private readonly XGraphics gfx;
+        public int finalAmount;
+        public ManagerPdf(PdfPage page, XGraphics gfx, PdfDocument document)
+        {
+            this.page = page;
+            this.gfx = gfx;
+            this.document = document;
+        }
+
+        public void HorizontalLine(int y)
+        {
+            XPen line = new XPen(XColors.DarkGreen, 3);
+            gfx.DrawLine(line, 0, y, page.Width, y);
+        }
+
+
+        public void CreateColumnOfData(string[] data, int x, int y, int distance)
+        {
+            foreach(string input in data)
+            {
+                WriteBoldText(input, 15, x, y, new XSolidBrush(XColors.DarkBlue));
+                x += distance;
+            }
+        }
+
+
+        public int CreateColumnOfAmount(List<string> data, int x, int y)
+        {
+            foreach (string input in data)
+            {
+                WriteText("$"+input, 12, x, y, new XSolidBrush(XColors.DarkBlue));
+                y += 20;
+                finalAmount += int.Parse(input);
+            }
+            return y;
+        }
+
+        public void DigitalSign(string name, int y)
+        {
+            XPen line = new XPen(XColors.Black, 1);
+            gfx.DrawLine(line, 390, y + 100, 500, y + 100);
+            WriteText($"Aclaración: {name}", 9, 390, y + 120, new XSolidBrush(XColors.Black));
+        }
+
+        public void WriteText(string text, int size, int xAxis, int yAxis, XSolidBrush brush)
+        {
+            var font = new XFont("Arial", size);
+
+            var textColor = brush;
+            var layout = new XRect(xAxis, yAxis, page.Width, page.Height);
+            var format = XStringFormats.TopLeft;
+
+            gfx.DrawString(text, font, textColor, layout, format);
+        }
+
+        public void WriteBoldText(string text, int size, int xAxis, int yAxis, XSolidBrush brush)
+        {
+            var font = new XFont("Arial", size, XFontStyle.Bold);
+
+            var textColor = brush;
+            var layout = new XRect(xAxis, yAxis, page.Width, page.Height);
+            var format = XStringFormats.TopLeft;
+
+            gfx.DrawString(text, font, textColor, layout, format);
+        }
+
+
+        public void CreateHeader(string name, string company, string invoiceNumber)
+        {
+            WriteBoldText("FACTURA", 50, 10, 20, new XSolidBrush(XColors.Black));
+
+            WriteText(name, 12, 10, 80, new XSolidBrush(XColors.Black));
+
+            CreateColumnOfData(new string[] { "FACTURAR A", "N° DE FACTURA" }, 10, 200, 360);
+
+            WriteText(invoiceNumber, 9, 500, 200, new XSolidBrush(XColors.Black));
+
+            WriteText(company, 9, 10, 220, new XSolidBrush(XColors.Black));
+            WriteText("FECHA", 12, 370, 220, new XSolidBrush(XColors.Black));
+            WriteText("25/01/2023", 9, 500, 220, new XSolidBrush(XColors.Black));
+        }
+        public void CreatePDF(string name, string company, List<string> amounts, string invoiceNumber, string path)
+        {
+            GlobalFontSettings.FontResolver = new FontResolver();
+            var document = new PdfDocument();
+            var page = document.AddPage();
+            var gfx = XGraphics.FromPdfPage(page);
+
+            CreateHeader(name, company, invoiceNumber);
+
+            HorizontalLine(250);
+            CreateColumnOfData(new string[] { "DESCRIPCIÓN", "IMPORTE" }, 10, 260, 490);
+
+            HorizontalLine(285);
+
+            int yFinalValue = CreateColumnOfAmount(amounts, 500, 300);
+
+            CreateColumnOfData(new string[] { "TOTAL", "$" + (finalAmount.ToString()) }, 370, yFinalValue + 30, 100);
+
+            DigitalSign(name, yFinalValue + 30);
+
+
+            document.Save(@$"{path}\invoice{invoiceNumber}.pdf");
+        }
+    }
+}
---
+++ Facturame/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Facturame;
+using PdfSharpCore.Drawing;
+using PdfSharpCore.Fonts;
+using PdfSharpCore.Pdf;
+using PdfSharpCore.Utils;
+using System.Reflection.Metadata;
+
+
+ManagerInput managerInput = new ManagerInput();
+Console.WriteLine("Bienvenido a Facturame! \n");
+Console.WriteLine("Para crear su factura necesitamos los siguientes datos:");
+
+
+string name = managerInput.LoadStringData("Ingrese su nombre y apellido: ");
+
+string company = managerInput.LoadStringData("Ingrese el nombre de la entidad a la cual le va a facturar: ");
+
+List<string> amounts = new List<string>();
+
+if(managerInput.LoadBoolData("¿Tiene varios montos a colocar en la misma factura? S/N: "))
+{
+    managerInput.LoadMultipleAmount(amounts);
+}
+else
+{
+    managerInput.LoadAmount(amounts);
+}
+string invoiceNumber = managerInput.LoadStringData("ingrese el número de factura: ");
+
+
+string path = managerInput.LoadStringData("La ubicación donde desea guarda la factura: ");
+
+GlobalFontSettings.FontResolver = new FontResolver();
+var document = new PdfDocument();
+var page = document.AddPage();
+var gfx = XGraphics.FromPdfPage(page);
+ManagerPdf manager = new ManagerPdf(page, gfx, document); 
+
+
+
+manager.WriteBoldText("FACTURA", 50, 10, 20, new XSolidBrush(XColors.Black));
+
+manager.WriteText(name, 12, 10, 80, new XSolidBrush(XColors.Black));
+
+manager.CreateColumnOfData(new string[] { "FACTURAR A", "N° DE FACTURA"}, 10, 200, 360);
+
+
+manager.WriteText(invoiceNumber, 9, 500, 200, new XSolidBrush(XColors.Black));
+
+
+manager.WriteText(company, 9, 10, 220, new XSolidBrush(XColors.Black));
+manager.WriteText("FECHA", 12, 370, 220, new XSolidBrush(XColors.Black));
+manager.WriteText("25/01/2023", 9, 500, 220, new XSolidBrush(XColors.Black));
+
+manager.HorizontalLine(250);
+manager.CreateColumnOfData(new string[] { "DESCRIPCIÓN", "IMPORTE" }, 10, 260, 490);
+
+manager.HorizontalLine(285);
+
+int yFinalValue = manager.CreateColumnOfAmount(amounts, 500, 300);
+
+manager.CreateColumnOfData(new string[] { "TOTAL", manager.finalAmount.ToString() }, 370, yFinalValue + 30, 100);
+
+manager.DigitalSign(name, yFinalValue + 30);
+
+
+document.Save(@$"{path}\invoice{invoiceNumber}.pdf");
+
+Console.WriteLine("Su fatctura fue creada con éxito!");
